f2cb927d947f721ec1ba4672a32f1bc69abb9364
fnolasco <fabio.nolasco@aig.com>
2020-10-20 15:59:20 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'aem-poc-multi-slot';
+}
